49e98304bf11d904c49f6eb05b4a0604c0a43e67
Matthew <matthew@soulanille.net>
2014-12-26 22:57:05 -0800

    forgot the launch script in the last commit

--- server/LaunchServer.sh
+++ server/LaunchServer.sh
@@ -1,2 +1,5 @@
-echo 'Ram:'
-read ram
+clear
+echo 'How much ram would you like to give this server in Mb?'
+read RAM
+
+  java -Xms512m -Xmx"$RAM"m -XX:PermSize=256m -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -jar CanaryMod.jar nogui
