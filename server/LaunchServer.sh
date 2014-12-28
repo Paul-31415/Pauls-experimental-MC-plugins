9004b890cb55631433cc04e7fb6ee9b1afb878ba
Paul Soulanille <paul@soulanille.net>
2014-12-27 21:08:00 -0800

    capitalized the 'b's

--- server/LaunchServer.sh
+++ server/LaunchServer.sh
@@ -1,16 +1,16 @@
 clear
-echo 'How much ram would you like to give this server? (defaults to Mb use b suffix for bytes)'
+echo 'How much ram would you like to give this server? (defaults to MB use B suffix for bytes)'
 read RAM
 ramend=${RAM:(${#RAM}-1)}
 #echo $ramend
 if [[ $ramend =~ [0-9] ]]; then 
-    echo $RAM'Mb'
+    echo $RAM'MB'
     java -Xms512m -Xmx"$RAM"m -XX:PermSize=256m -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -jar CanaryMod.jar nogui
 elif [[ $ramend =~ [kKmMgGtTpPeEzZyY] ]]; then
-    echo ${RAM:0:(${#RAM}-1)}$ramend'b'
+    echo ${RAM:0:(${#RAM}-1)}$ramend'B'
     java -Xms512m -Xmx"$RAM" -XX:PermSize=256m -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -jar CanaryMod.jar nogui
-elif [[ $ramend =~ [b] ]]; then
-    echo ${RAM:0:(${#RAM}-1)}'b'
+elif [[ $ramend =~ [bB] ]]; then
+    echo ${RAM:0:(${#RAM}-1)}'B'
     java -Xms512m -Xmx"${RAM:0:(${#RAM}-1)}" -XX:PermSize=256m -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -jar CanaryMod.jar nogui
 else
     echo 'Error: unknown/disalowed si-prefix:"'$ramend'"'
